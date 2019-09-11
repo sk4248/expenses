9f27441e64054ff32bef9fb813ff61726270940e
sreekanth korampally <sreekanthkorampally@sreekanths-mbp.home>
2019-09-11 00:05:06 -0400

    trying to fix the firebase issue

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
+import 'firebase/app';
+import 'firebase/firestore';
 import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
